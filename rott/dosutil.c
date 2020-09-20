175cff0bd053b6704ef80808b31cfbdf8b286e04
zerojay <jay@zerojay.com>
2020-09-20 19:48:16 -0400

    Revert GCW Zero changes.

--- rott/dosutil.c
+++ rott/dosutil.c
@@ -197,7 +197,7 @@ void DisplayTextSplash(byte *text, int l)
 	printf ("\033[m");
 }
 
-#if !defined(__CYGWIN__) && !defined(__MINGW32__) && !defined(_GCW_)
+#if !defined(__CYGWIN__) && !defined(__MINGW32__)
 #include <execinfo.h>
 
 void print_stack (int level)
